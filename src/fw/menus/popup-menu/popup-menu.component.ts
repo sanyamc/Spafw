ae2d29414cf029b406a942b9322b1ac72e69b81a
Sanyam Chaudhary <sanyamc@microsoft.com>
2017-12-18 07:11:58 -0800

    mat menu item

--- src/fw/menus/popup-menu/popup-menu.component.ts
+++ src/fw/menus/popup-menu/popup-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
 
 import { MenuItem, MenuService } from '../../services/menu.service';
 
